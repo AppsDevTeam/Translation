45554119adc2cd6c49a1a7fcc14644696bea5594
Michal Lohnisky <michal@appsdevteam.com>
2015-01-23 15:48:41 +0100

    oneSkyApp scripty

--- example-scripts/translator-push.sh
+++ example-scripts/translator-push.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# nahraje překlady na oneSkyApp pro všechny jazyky
+# nahraje překlady na oneSkyApp pro cs
 php web/index.php kdyby:translation-extract --catalogue-language="cs" --output-format="po" --exclude-prefix-file="./translator-exclude-prefix" --onesky-upload
-php web/index.php kdyby:translation-extract --catalogue-language="en" --output-format="po" --exclude-prefix-file="./translator-exclude-prefix" --onesky-upload
-php web/index.php kdyby:translation-extract --catalogue-language="fr" --output-format="po" --exclude-prefix-file="./translator-exclude-prefix" --onesky-upload
-php web/index.php kdyby:translation-extract --catalogue-language="de" --output-format="po" --exclude-prefix-file="./translator-exclude-prefix" --onesky-upload
