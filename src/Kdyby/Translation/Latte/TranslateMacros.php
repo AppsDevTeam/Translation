fea7da01cd323c01f89c07f67742d62e3d04225a
Tomáš Kudělka <thorewi@gmail.com>
2017-08-11 20:53:51 +0200

    nette 2.4 compatibility

--- src/Kdyby/Translation/Latte/TranslateMacros.php
+++ src/Kdyby/Translation/Latte/TranslateMacros.php
@@ -48,18 +48,21 @@ class TranslateMacros extends MacroSet
 	public function macroTranslate(MacroNode $node, PhpWriter $writer)
 	{
 		if ($node->closing) {
-			return $writer->write('echo %modify($template->translate(ob_get_clean()))');
-
-		} elseif ($node->isEmpty = ($node->args !== '')) {
-			if ($this->containsOnlyOneWord($node)) {
-				return $writer->write('echo %modify($template->translate(%node.word))');
-
+			if (strpos($node->content, '<?php') === FALSE) {
+				$value = var_export($node->content, TRUE);
+				$node->content = '';
 			} else {
-				return $writer->write('echo %modify($template->translate(%node.word, %node.args))');
+				$node->openingCode = '<?php ob_start(function () {}) ?>' . $node->openingCode;
+				$value = 'ob_get_clean()';
 			}
-
-		} else {
-			return 'ob_start()';
+			return $writer->write('$_fi = new LR\FilterInfo(%var); echo %modifyContent($this->filters->filterContent("translate", $_fi, %raw))', $node->context[0], $value);
+		} elseif ($node->args !== '') {
+			$node->empty = TRUE;
+			if ($this->containsOnlyOneWord($node)) {
+				return $writer->write('echo %modify(call_user_func($this->filters->translate, %node.word))');
+			} else {
+				return $writer->write('echo %modify(call_user_func($this->filters->translate, %node.word, %node.args))');
+			}
 		}
 	}
 
